bedd5d34fbe2679616340ac08a05d9d5809ed224
BananaLF <864685021@qq.com>
2019-10-09 23:25:35 +0800

    最新价格不同与我的价格按照步长加模式进行自动报价.(单价模式)

--- assistant.js
+++ assistant.js
@@ -1,7 +1,9 @@
 var syncStart = false
-var localDate = 0
-var oldTime = 0
-var oldPrice = 0
+var syncPrice = 0
+var myPrice = 0
+var appMode = "单价"
+var interval = 0
+var maxPrice = 0
 //notifySyncData()
 
 chrome.browserAction.onClicked.addListener(function(tab) {
@@ -12,14 +14,17 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 $(function(){
     var state = $('#state');
     $('#startSync').click(function () {//给对象绑定事件
+        interval = parseInt(document.getElementById("intervalText").value)
+        maxPrice = parseInt(document.getElementById("maxPriceText").value)
         startSync()
     });
     $('#stopSync').click(function () {//给对象绑定事件
+        updateUI()
         stopSync()
     });
     $('#singleSend').click(function () {
         console.log("start to send")
-        notifySendPrice()
+        notifySendPrice(1)
     })
 })
 
@@ -41,13 +46,19 @@ function stopSync() {
 }
 
 //通知contentScript 向目标界面发送最后价格
-function notifySendPrice() {
+function notifySendPrice(price) {
     chrome.tabs.query({active:true, currentWindow:true}, function (tab) {//获取当前tab
         //向tab发送请求
         chrome.tabs.sendMessage(tab[0].id, {
             action: "start",
+            price:price,
+            mode:appMode,
         }, function (response) {
             console.log(response);
+            if (response.state != '发送成功') {
+                syncStart = false
+                alert(response.state)
+            }
         });
     });
 }
@@ -60,58 +71,73 @@ function notifySyncData() {
             action: "syncData",
         }, function (response) {
             console.log("syncData",response)
-            updateData(response.lastTime,response.currentPrice)
-            updateUI()
+            updateData(response.currentPrice,response.ownPrice,response.mode)
         });
     });
 }
 
 //定时器, 1ms执行一次. 时间消耗完或主动暂停时停止. 最后500ms内自动发送最后价格然后停止
 function TimeDown() {
-    if (!syncStart || localDate <= 0) {
+    if (!syncStart) {
+        updateUI()
         console.log("sync had stop")
-        return;
-    }
-
-    if (localDate < 700) {
-        notifySendPrice()
-        console.log("sync had stop")
-        alert("发送成功")
         return;
     }
     notifySyncData()
-    localDate -= 1
-    //延迟一秒执行自己
     setTimeout(function () {
         TimeDown();
-    }, 1)
+    }, 100)
 }
 
-//更新本地数据,价格是多少就是多少.
-// 由于服务器时间(syncTime)可能和本地时间不同因此本地时间需更新为服务器时间. 如果服务器时间并未更新的话,本地时间还是正常倒计时.
-// TODO: 由于网络延时的原因收到的服务器时间小于本地时间,因此本地时间 = 收到的服务器时间+时延. 时延目前猜测大于在20~50ms间,因此需要进行本地时间的更新需要加入时间目前并未做
-function updateData(syncTime,syncPrice) {
-    if (oldTime == 0) {
-        oldTime = syncTime
-        localDate = syncTime
-    } else if (oldTime == syncTime){
-        return;
-    } else if (localDate == syncTime) {
-        return;
-    } else if (localDate > syncTime) {
-        console.log("localDate > syncTime :",localDate-syncTime)
-        oldTime = syncTime
-        localDate = syncTime
-    } else if (localDate < syncTime) {
-        console.log("localDate < syncTime :",localDate-syncTime)
-        oldTime = syncTime
-        localDate = syncTime
-    }
-    oldPrice = syncPrice
+
+function updateData(currentPrice,ownPrice,mode) {
+    syncPrice = currentPrice
+    myPrice = ownPrice
+    appMode = mode
+    autoCheck()
+    updateUI()
 }
 
 // 更新UI显示
 function updateUI() {
-    document.getElementById("currentTimeText").innerText = localDate
-    document.getElementById("currentPriceText").innerText = oldPrice
+    document.getElementById("appModeText").innerText = appMode
+    document.getElementById("currentPriceText").innerText = syncPrice
+    document.getElementById("myPriceText").innerText = myPrice
+    if (syncStart) {
+        document.getElementById("appStatusText").innerText = "进行中"
+    } else {
+        document.getElementById("appStatusText").innerText = "已停止"
+    }
+}
+
+function autoCheck() {
+    if (syncPrice != myPrice) {
+        if (appMode == "单价") {
+            //发送
+            var priceArray= new Array();
+            priceArray=syncPrice.split("/");
+            if (priceArray.length != 2) {
+                syncStart = false
+                alert("未知错误,停止自动报价")
+                return
+            }
+
+            var preparePrice = parseFloat(priceArray[1])
+            preparePrice = preparePrice + interval
+            if (preparePrice > maxPrice) {
+                syncStart = false
+                alert("价格超过预期,停止自动报价")
+                return;
+            }
+            notifySendPrice(preparePrice)
+        } else {
+            syncStart = false
+            alert("未知模式类型,停止自动报价")
+            return;
+        }
+    } else {
+        syncStart = false
+        alert("数据同步错误,停止自动报价")
+        return;
+    }
 }
--- contentScript.js
+++ contentScript.js
@@ -2,16 +2,36 @@ chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         var contentDocument = document.getElementById("external").contentDocument
         if (request.action == "start") {
+            var mode = contentDocument.getElementById("bidMode").innerText
+            if (request.mode == mode && mode == '单价' ){
+                var table = contentDocument.getElementById("packDetailTab")
+                if (table.rows.length != 4) {
+                    sendResponse({state:'发送失败,无法找到价格输入DOM'});
+                    return
+                }
+                var input = contentDocument.getElementsByClassName("TB_priceInput")
+                if (input.length != 1){
+                    sendResponse({state:'发送失败,价格输入DOM数不唯一'});
+                    return
+                }
+                if (input[0].tagName != 'INPUT'){
+                    sendResponse({state:'发送失败,无法找到价格输入DOM,未知标签类型'+input[0].tagName});
+                }
+                input[0].value = request.price
+            } else {
+                sendResponse({state:'发送失败,未知模式'});
+            }
             contentDocument.getElementById("bidBtnId").disabled = false
-            contentDocument.getElementById("bidBtnId").click()
+            //TODO 需要测试后 才开放自动点击按钮
+            //contentDocument.getElementById("bidBtnId").click()
             sendResponse({state:'发送成功'});
         }
         if (request.action == "syncData") {
-            var ssPack = parseInt(contentDocument.getElementById("countDown_counting").innerText) * 1000
+            //var ssPack = parseInt(contentDocument.getElementById("countDown_counting").innerText) * 1000
             var currentPrice = contentDocument.getElementById("currentPrice").innerText
             var ownPrice = contentDocument.getElementById("currentPrice_bidder").innerText
             var mode = contentDocument.getElementById("bidMode").innerText
-            sendResponse({lastTime:ssPack,currentPrice:currentPrice,ownPrice:ownPrice,mod:mode});
+            sendResponse({currentPrice:currentPrice,ownPrice:ownPrice,mode:mode});
         }
     }
 );
